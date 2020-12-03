e7112dbb8714840b1929773ba35f9f80d7aadadc
Kristopher Koch <Kristopher.Koch@dowjones.com>
2020-12-02 18:49:58 -0700

    hacky certificate stuff

--- MayaKochRealtor/MayaKochRealtor.WebHost/Startup.cs
+++ MayaKochRealtor/MayaKochRealtor.WebHost/Startup.cs
@@ -29,6 +29,7 @@ namespace MayaKochRealtor.WebHost
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
